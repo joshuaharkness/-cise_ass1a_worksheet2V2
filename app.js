10e671dbc1c8330a4d4ec635e263458d3bb4508e
hhm4286 <83788989+hhm4286@users.noreply.github.com>
2022-04-01 15:53:14 +1300

    Attempt at deployment

--- app.js
+++ app.js
@@ -23,4 +23,13 @@ app.use('/api/books', books);
 
 const port = process.env.PORT || 8082;
 
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
